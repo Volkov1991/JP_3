aee06845a3a1ce14252b536ff453013f8735070d
Volkov <vad.volkov91@gmail.com>
2024-04-10 00:23:50 +0700

    upd 10.04

--- src/main/java/org/example/Randoms.java
+++ src/main/java/org/example/Randoms.java
@@ -5,31 +5,38 @@ import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    private final int min;
-    private final int max;
-    private final PrimitiveIterator.OfInt randomIterator;
+    private int min;
+    private int max;
+    //private final PrimitiveIterator.OfInt randomIterator;
     protected Random random;
 
     public Randoms(int min, int max) {
-        randomIterator = new Random().ints(min, (max + 1)).iterator();
+       // randomIterator = new Random().ints(min, (max + 1)).iterator();
         this.min = min;
         this.max = max;
+        this.random = new Random();
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
-            @Override
-            public boolean hasNext() {
-                return true;
-            }
-
-            @Override
-            public Integer next() {
-                return randomIterator.nextInt();
-            }
-        };
+        return new randomIterator();
     }
+    public class randomIterator implements Iterator<Integer> {
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(max - min + 1) + min;
+        }
+
+    }
+
+
+
+
 
     //...
 }
